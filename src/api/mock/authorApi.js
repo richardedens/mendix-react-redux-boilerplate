8ca9bd8b9df63708c9c88741972362d40d2ec587
Richard Edens <richardedens@outlook.com>
2018-11-30 13:46:40 +0100

    Inital commit

--- src/api/mock/authorApi.js
+++ src/api/mock/authorApi.js
@@ -5,9 +5,9 @@ import delay from './delay';
 // All calls return promises.
 const authors = [
   {
-    id: 'cory-house',
-    firstName: 'Cory',
-    lastName: 'House'
+    id: 'edens-richard',
+    firstName: 'Edens',
+    lastName: 'Richard'
   },
   {
     id: 'scott-allen',
